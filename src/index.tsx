3c8eed46ec6634fd29292424ae5b2b0c521b2817
Ismael Figueroa <ifigueroap@gmail.com>
2023-04-03 23:38:49 -0400

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
@@ -21,3 +24,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+defineCustomElements(window);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,47 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
+import React, { useState } from 'react';
+
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import {
+    IonButton, IonContent, IonHeader, IonLoading, IonPage, IonTitle, IonToolbar
+} from '@ionic/react';
+
 const Home: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+
+  const takePicture = async () => {
+    setLoading(true);
+    try {
+      const image: Photo = await Camera.getPhoto({
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Prompt,
+        quality: 100,
+        width: 512,
+        height: 512,
+        allowEditing: false,
+        promptLabelCancel: "Cancelar",
+        promptLabelHeader: "Foto",
+        promptLabelPhoto: "Desde Mis Fotos",
+        promptLabelPicture: "Tomar Foto",
+      });
+      console.log("image");
+      console.log(image);
+
+      if (image && image.dataUrl) {
+        console.log("doing something");
+      }
+    } catch (exc) {
+      console.log("foobarbaz");
+      console.log(exc);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <IonPage>
+      <IonLoading isOpen={loading} />
       <IonHeader>
         <IonToolbar>
           <IonTitle>Blank</IonTitle>
@@ -16,7 +53,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={() => takePicture()}></IonButton>
       </IonContent>
     </IonPage>
   );
